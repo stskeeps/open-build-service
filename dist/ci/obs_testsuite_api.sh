0b975796a3b4d1558d85a98e0e988a721ac0efc0
Sascha Peilicke <saschpe@suse.de>
2011-08-25 12:41:20 +0200

    [dist/ci] Add CI templates to allow Jenkins jobs against the 2.3 branch.
    Our Jenkins jobs directly execute those scripts from their git checkout,
    thus they're needed here (and in any other branch we may want to test).

---
+++ dist/ci/obs_testsuite_api.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+#
+# This script runs both API unit and integration tests and produces coverage
+# and todo/fixme reports as well as code statistics.
+#
+
+###############################################################################
+# Job configuration template
+###############################################################################
+#
+# Project name: obs_testsuite_api
+# Description:
+#   OBS API testsuite on git master branch.
+#
+#   Updates source code repository and sets up a working environment for all
+#   further tasks. Runs unit and integration tests generated coverage reports.
+#
+# Source Code Management:
+#   Git:
+#     Repositories: git://gitorious.org/opensuse/build-service.git
+#     Branches to build: master
+#
+# Build Triggers:
+#   Poll SCM:
+#     Schedule: */5 * * * *
+#
+# Build:
+#   Execute shell:
+#     Command: sh dist/ci/obs_testsuite_api.sh
+#
+# Post Build Actions:
+#   Archive the artifacts:
+#     Files to archive: **/*
+#     Discard all but the last successful/stable artifact to save disk space: 1
+#   Publish JUnit test result report:
+#     Test report XMLs: src/api/results/*.xml
+#   Publish Rails Notes report: 1
+#     Rake working directory: src/api
+#   Publish Rails stats report: 1
+#     Rake working directory: src/api
+#   Publish Rcov report:
+#     Rcov report directory:  src/api/coverage/test
+#
+
+###############################################################################
+# Script content for 'Build' step
+###############################################################################
+#
+# Either invoke as described above or copy into an 'Execute shell' 'Command'.
+#
+
+echo "Setup git submodules"
+git submodule init
+git submodule update
+
+echo "Setup backend configuration template"
+sed -i -e "s|my \$hostname = .*$|my \$hostname = 'localhost';|" \
+       -e "s|our \$bsuser = 'obsrun';|our \$bsuser = 'jenkins';|" \
+       -e "s|our \$bsgroup = 'obsrun';|our \$bsgroup = 'jenkins';|" src/backend/BSConfig.pm.template
+cp src/backend/BSConfig.pm.template src/backend/BSConfig.pm
+
+echo "Enter API rails root"
+cd src/api
+
+echo "Setup database configuration"
+cp config/database.yml.example config/database.yml
+sed -i "s|database: api|database: ci_api|" config/database.yml
+
+echo "Setup additional configuration"
+cp config/options.yml.example config/options.yml
+
+echo "Install missing gems locally and fetch rails_rcov"
+rake gems:install
+ruby script/plugin install http://svn.codahale.com/rails_rcov
+
+echo "Set environment variables"
+export CI_REPORTS=results
+export RAILS_ENV=test
+
+echo "Initialize test database, run migrations, load seed data"
+rake db:drop db:create db:setup db:migrate
+
+echo "Prepare for rcov"
+[ -d "coverage" ] && rm -rf coverage
+mkdir coverage
+
+echo "Invoke rake"
+rake ci:setup:testunit test:test:rcov --trace RCOV_PARAMS="--aggregate coverage/aggregate.data"
+
+echo "Remove unneded files to save disc space"
+rm -rf src/api/log/* \
+       src/api/tmp/*
---
+++ dist/ci/obs_testsuite_webui.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+#
+# This script runs both WebUI unit and integration tests and produces coverage
+# and todo/fixme reports as well as code statistics.
+#
+
+###############################################################################
+# Job configuration template
+###############################################################################
+#
+# Project name: obs_testsuite_webui
+# Description:
+#   OBS WebUI testsuite on git master branch.
+#
+#   Runs unit and integration tests, generated coverage reports.
+#
+# Build Triggers:
+#   Build after other projects are built:
+#     Project names: obs_testsuite_api
+#
+# Build:
+#   Copy artifacts from another project:
+#     Project name: obs_testsuite_api
+#     Artifacts to copy: **/*
+#   Execute shell:
+#     Command: sh dist/ci/obs_testsuite_webui.sh
+#
+# Post Build Actions:
+#   Publish JUnit test result report:
+#     Test report XMLs: src/webui/results/*.xml
+#   Publish Rails Notes report: 1
+#     Rake working directory: src/webui
+#   Publish Rails stats report: 1
+#     Rake working directory: src/webui
+#   Publish Rcov report:
+#     Rcov report directory:  src/webui/coverage/test
+#
+
+###############################################################################
+# Script content for 'Build' step
+###############################################################################
+#
+# Either invoke as described above or copy into an 'Execute shell' 'Command'.
+#
+
+echo "Enter WebUI rails root"
+cd src/webui
+
+echo "Setup database configuration"
+cp config/database.yml.example config/database.yml
+
+echo "Setup additional configuration"
+cp config/options.yml.example config/options.yml
+
+echo "Install missing gems locally and fetch rails_rcov"
+#rake gems:install # TODO: Fix webui to make this work!
+ruby script/plugin install http://svn.codahale.com/rails_rcov
+
+echo "Set environment variables"
+export CI_REPORTS=results
+export RAILS_ENV=test
+
+echo "Fix executable bits broken by 'Copy Artifacts' plugin"
+chmod +x script/start_test_api
+
+echo "Initialize test database, run migrations, load seed data"
+rake db:drop db:create db:migrate
+
+echo "Prepare for rcov"
+[ -d "coverage" ] && rm -rf coverage
+mkdir coverage
+
+echo "Invoke rake"
+rake ci:setup:testunit test:test:rcov --trace RCOV_PARAMS="--aggregate coverage/aggregate.data"
+
+echo "Remove unneded files to save disc space"
+rm -rf src/api/log/* \
+       src/api/tmp/*
